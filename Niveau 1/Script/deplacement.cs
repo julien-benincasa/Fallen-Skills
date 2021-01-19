4fed740b73012911f33d89f13fd211ae398e3452
julien-benincasa <60385075+julien-benincasa@users.noreply.github.com>
2021-01-19 11:25:41 +0100

    Add files via upload

---
+++ Niveau 1/Script/deplacement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deplacement : MonoBehaviour{
+
+	public float speed = 0.001f;
+	public float jumph = 50f;
+
+	void Start () { print ("Start"); }
+
+	void Update () {
+
+		Vector3 dp = new Vector3();
+
+		if (Input.GetKey (KeyCode.Q)) {
+			dp.x -= speed;
+		}
+
+		if (Input.GetKey (KeyCode.D)) {
+			dp.x += speed;	
+		}
+		if (Input.GetKey (KeyCode.Z)) {
+			dp.y += speed;	
+		}
+
+		transform.position += dp;
+	}
+}
